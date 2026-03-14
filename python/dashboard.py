5f3517a20146a79067b61ab791d0944ca43ca966
Shubham Nagpure <shubhamnagpure989@gmail.com>
2026-03-14 21:10:15 +0530

    SentriLock AI v1 - Intruder Detection System with Alarm and Camera Capture
    SentriLock AI v1 - Intruder Detection System with Alarm and Camera Capture

--- python/dashboard.py
+++ python/dashboard.py
@@ -1,7 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 from wifi_scanner import scan_wifi
 
 app = Flask(__name__)
+
+# absolute path to intruder folder
+INTRUDER_FOLDER = os.path.join(os.getcwd(), "intruders")
+
+
+# serve intruder images
+@app.route("/intruders/<path:filename>")
+def intruder_files(filename):
+    return send_from_directory(INTRUDER_FOLDER, filename)
+
+
+# get latest intruder image
+def get_latest_intruder():
+
+    if not os.path.exists(INTRUDER_FOLDER):
+        return None
+
+    files = os.listdir(INTRUDER_FOLDER)
+
+    if not files:
+        return None
+
+    latest = max(
+        files,
+        key=lambda x: os.path.getctime(os.path.join(INTRUDER_FOLDER, x))
+    )
+
+    return latest
 
 
 @app.route("/")
@@ -9,7 +38,13 @@ def home():
 
     devices = scan_wifi()
 
-    return render_template("dashboard.html", devices=devices)
+    intruder_image = get_latest_intruder()
+
+    return render_template(
+        "dashboard.html",
+        devices=devices,
+        intruder_image=intruder_image
+    )
 
 
 if __name__ == "__main__":
